afa80edf630d0687f10814e5d5d3c1de191d5f9e
Ruben Rubio Rey <ruben@manageacloud.com>
2015-01-10 13:26:40 +1100

    Fixed problem with refactoring

--- maccli/__init__.py
+++ maccli/__init__.py
@@ -4,13 +4,13 @@ import logging
 import maccli.dao.api_auth
 
 
-__version__ = '1.0a0'
+__version__ = '1.0a2'
 
 # : The username used to authenticate with the API
-user = dao.api_auth.load_from_file()[0] or os.environ.get('MAC_USER', None)
+user = maccli.dao.api_auth.load_from_file()[0] or os.environ.get('MAC_USER', None)
 
 #: The ApiKey used to authenticate with the API
-apikey = dao.api_auth.load_from_file()[1] or os.environ.get('MAC_APIKEY', None)
+apikey = maccli.dao.api_auth.load_from_file()[1] or os.environ.get('MAC_APIKEY', None)
 
 #: The API endpoint to use
 base_url = os.environ.get('MAC_BASE_URL', "https://manageacloud.com/api/v1/")
