06d2c5710dd26b06bfb95cf03373622b1c5be575
Ruben Rubio Rey <ruben@manageacloud.com>
2015-09-17 13:36:59 +1000

    Addded CMD

--- maccli/__init__.py
+++ maccli/__init__.py
@@ -4,7 +4,7 @@ import logging
 import maccli.dao.api_auth
 
 
-__version__ = '0.9.9'
+__version__ = '0.9.10'
 
 # : The username used to authenticate with the API
 user = maccli.dao.api_auth.load_from_file()[0] or os.environ.get('MAC_USER', None)
